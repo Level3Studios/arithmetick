9ec975f9881c2bc2e3428d35aaad69a27e7df719
Sal Aldana <saldana@us.imshealth.com>
2022-08-24 15:54:04 -0700

    🕸️ Web App Setup
    Setup the Web App using Vite with Svelte-TypScript and then added TailwindCSS. Also started working on Category Model.

---
+++ web/arithmetick/src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Router, Route, Link } from "svelte-navigator";
+</script>
+
+<Router>
+  <header>
+    <h1>Arithmetick</h1>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="converter">Converter</Link>
+    </nav>
+  </header>
+
+  <main>
+    <Route path="/">
+      <h3>Category Page</h3>
+    </Route>
+
+    <Route path="converter">
+      <h3>Converter View</h3>
+    </Route>
+  </main>
+  <footer>
+    <h6>Copyright 2022</h6>
+  </footer>
+</Router>
